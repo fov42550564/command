cd7695e12bac199df21fd5bc257a72b554dc10b1
fov42550564 <42550564@qq.com>
2018-04-17 21:09:36 +0800

    修正update

--- linux/bash/.mongorc.js
+++ linux/bash/.mongorc.js
@@ -12,7 +12,33 @@ Object.defineProperty(this, "ls", {
 const defaultFind = DBCollection.prototype.find;
 DBCollection.prototype.find = function (query, fields, limit, skip, batchSize, options) {
     if (typeof query === 'string') {
-        return defaultFind.call(this, {_id: ObjectId(query)}, fields, limit, skip, batchSize, options);
+        query = {_id: ObjectId(query)};
     }
     return defaultFind.call(this, query, fields, limit, skip, batchSize, options);
+};
+
+
+const defaultUpdate =  DBCollection.prototype.update;
+DBCollection.prototype.update = function (query, obj, upsert, multi) {
+    if (typeof query === 'string') {
+        query = {_id: ObjectId(query)};
+    }
+    if (!obj['$set'] || !obj['$unset']) {
+        var needUnset = true;
+        Object.keys(obj).forEach((k)=>{
+            if (obj[k]!==null) {
+                needUnset = false;
+            }
+        });
+
+        if (needUnset) {
+            Object.keys(obj).forEach((k)=>{
+                obj[k] = 1;
+            });
+            obj = {$unset: obj};
+        } else {
+            obj = {$set: obj};
+        }
+    }
+    return defaultUpdate.call(this, query, obj, upsert, multi);
 };
