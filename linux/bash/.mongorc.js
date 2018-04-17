3e9ff8f7155072e8986129bd6ce16c23d5b65d48
fov42550564 <42550564@qq.com>
2018-04-17 17:29:10 +0800

    update mongorc

--- linux/bash/.mongorc.js
+++ linux/bash/.mongorc.js
@@ -9,26 +9,10 @@ Object.defineProperty(this, "ls", {
     },
 });
 
-
-DBCollection.prototype.findById = function (id, fields, limit, skip, batchSize, options) {
-    var cursor = new DBQuery(
-        this._mongo,
-        this._db,
-        this,
-        this._fullName,
-        this._massageObject({_id: ObjectId(id)}),
-        fields,
-        limit,
-        skip,
-        batchSize,
-        options || this.getQueryOptions()
-    );
-
-    var connObj = this.getMongo();
-    var readPrefMode = connObj.getReadPrefMode();
-    if (readPrefMode != null) {
-        cursor.readPref(readPrefMode, connObj.getReadPrefTagSet());
+const defaultFind = DBCollection.prototype.find;
+DBCollection.prototype.find = function (query, fields, limit, skip, batchSize, options) {
+    if (typeof query === 'string') {
+        return defaultFind.call(this, {_id: ObjectId(query)}, fields, limit, skip, batchSize, options);
     }
-
-    return cursor;
+    return defaultFind.call(this, query, fields, limit, skip, batchSize, options);
 };
