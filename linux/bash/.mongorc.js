6fcea249da7071209aa20ae97bef19c833414b8e
fov42550564 <42550564@qq.com>
2018-04-20 15:37:11 +0800

    update mongorc.js

--- linux/bash/.mongorc.js
+++ linux/bash/.mongorc.js
@@ -1,11 +1,15 @@
+const FILTERS = {
+    roadmaps: ['sendDoorList'],
+};
+
 String.prototype.padRight = function(total, pad) {
     return (this+Array(total).join(pad || ' ')).slice(0, total);
 }
 
 function showList(list) {
-    let cnt = 0;
-    let line = '';
-    for (let i in list) {
+    var cnt = 0;
+    var line = '';
+    for (var i in list) {
         const item = list[i];
         if (cnt++ < 4) {
             line += '    ' + item.padRight(30);
@@ -24,6 +28,19 @@ function id(s) {
     }
     return s;
 }
+
+Object.defineProperty(this, "_h", {
+    get: function() {
+        print('show self defined command:');
+        print('     ls:     show databases and collections');
+        print('     _:      switch pretty shell');
+        print('     _n:     set shellBatchSize, e.g: _n=10');
+        print('     _host:  show host');
+        print('     _189:   switch to 189 server');
+        print('     find:   find(id/str/obj, \'[-]xx xx ...\'/{xx:1, ...})');
+        print('     update: find(id/obj, {xx:xx, ...})');
+    },
+});
 
 Object.defineProperty(this, "ls", {
     get: function() {
@@ -58,13 +75,13 @@ Object.defineProperty(this, "_n", {
     },
 });
 
-Object.defineProperty(this, "__host", {
+Object.defineProperty(this, "_host", {
     get: function() {
         print(db.getMongo().host);
     },
 });
 
-Object.defineProperty(this, "__189", {
+Object.defineProperty(this, "_189", {
     get: function() {
         if (db.getMongo().host === '127.0.0.1:27017') {
             db = new Mongo("192.168.1.189").getDB('pdshop');
@@ -78,27 +95,34 @@ Object.defineProperty(this, "__189", {
 
 const defaultFind = DBCollection.prototype.find;
 DBCollection.prototype.find = function (query, fields, limit, skip, batchSize, options) {
-    let obj = {};
-    let keys = Object.keys(defaultFind.call(this, {}, {_id: 0, __v: 0}, 1, 0).toArray()[0]||{});
-    if (typeof fields === 'string') {
+    const name = this.getName();
+    var obj = {};
+    var keys = Object.keys(defaultFind.call(this, {}, {_id: 0, __v: 0}, 1, 0).toArray()[0]||{}).filter(o=>{
+        if (!FILTERS[name] || FILTERS[name].indexOf(o) === -1) {
+            return true;
+        }
+    });
+
+    if (fields && typeof fields === 'string') {
         fields = fields.split(' ').filter(o=>!!o);
-    }
-    if (fields instanceof Array) {
-        fields = fields.map(o=>{
-            let unmatch = false;
-            if (o[0] === '-') {
-                unmatch = true;
-                o = o.slice(1);
-            }
-            return { unmatch, reg: new RegExp(o)};
-        });
+        var match = true;
+        if (fields[0][0] === '-') {
+            fields[0] = fields[0].slice(1);
+            match = false;
+        }
         keys.filter(o => {
-            for (let f of fields) {
-                if (f.reg.test(o)) {
+            for (var f of fields) {
+                if (new RegExp(f).test(o)) {
                     return true;
                 }
             }
-        }).forEach(o=>obj[o]=1);
+        }).forEach(o=>obj[o]= match ? 1 : 0);
+        if (!match) {
+            obj['__v'] = 0;
+            if (FILTERS[name]) {
+                FILTERS[name].forEach(o=>obj[o] = 0);
+            }
+        }
     } else {
         obj = fields;
     }
