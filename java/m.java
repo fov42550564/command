05a21f6fc48440b36dad117f3db1e0132af6a5ea
fov42550564 <42550564@qq.com>
2019-06-26 17:58:29 +0800

    fix m.java

--- java/m.java
+++ java/m.java
@@ -3,12 +3,22 @@ import java.lang.reflect.Method;
 import java.util.*;
 import java.util.regex.Pattern;
 
+import static com.remobile.moment.Moment.moment;
 
 public class m {
     // 默认值
     public static <T> T ifNull(T value, T defaultValue) {
-        return value == null ? defaultValue : value;
+        return null == value ? defaultValue : value;
     }
+
+    //字符串转日期
+    public static Date date(String text) {
+        if (null == text) {
+            return null;
+        }
+        return moment(text).toDate();
+    }
+
     // 正则表达式检测
     public static Boolean test(String str, String reg) {
         if (null == str) {
@@ -17,22 +27,34 @@ public class m {
         return Pattern.compile(reg).matcher(str).find();
     }
     // 生成一个List
-    public static List<Object> list(Object... args) {
-        List<Object> list = new ArrayList();
-        for (Object arg : args) {
+    public static <T> List<T> list(T... args) {
+        List<T> list = new ArrayList();
+        for (T arg : args) {
             list.add(arg);
         }
         return list;
     }
+
     // 生成一个Map
     public static Object object(Object... args) {
         Map context = new HashMap();
-        int count  = args.length - 2;
-        for (int i = 0; i <= count; i+=2) {
-            context.put(args[i], args[i+1]);
+        int count = args.length - 2;
+        for (int i = 0; i <= count; i += 2) {
+            context.put(args[i], args[i + 1]);
         }
         return context;
     }
+
+    // 生成一个JSON
+    public static JSONObject json(Object... args) {
+        JSONObject context = new JSONObject();
+        int count = args.length - 2;
+        for (int i = 0; i <= count; i += 2) {
+            context.put((String) args[i], args[i + 1]);
+        }
+        return context;
+    }
+
     // 将类转化为Map
     public static Map object(Object obj) {
         Map context = new HashMap();
@@ -44,9 +66,11 @@ public class m {
                 context.put(field.getName(), field.get(obj));
             }
             return context;
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
         return context;
     }
+
     // 将Map合并到Map
     public static Map merge(Map map, Object... args) {
         int count = args.length - 2;
@@ -55,15 +79,17 @@ public class m {
         }
         return map;
     }
+
     // 将Map合并到Object
     public static Map merge(Object object, Object... args) {
-        Map map = m.object(object);
+        Map map = object(object);
         int count = args.length - 2;
         for (int i = 0; i <= count; i += 2) {
             map.put(args[i], args[i + 1]);
         }
         return map;
     }
+
     // 去除Map中的某些值
     public static Map omit(Map map, String... args) {
         for (String arg : args) {
@@ -71,6 +97,7 @@ public class m {
         }
         return map;
     }
+
     // 提取Collections中的某一个值
     public static <T> List<T> concat(List<T> list, List<T> list1) {
         for (T item : list1) {
@@ -78,6 +105,7 @@ public class m {
         }
         return list;
     }
+
     // 通过key值获取Object的值
     public static Object get(Object obj, String key) {
         try {
@@ -89,27 +117,30 @@ public class m {
         }
         return null;
     }
+
     // 提取Collections中的某一个值
     public static <T, M> List<T> pick(List<M> list, String key) {
         List<T> l = new ArrayList<T>();
         for (M item : list) {
-            T t = (T)m.get(item, key);
+            T t = (T) get(item, key);
             if (null != t) {
                 l.add(t);
             }
         }
         return l;
     }
+
     // 判断字符串相等
     public static Boolean eq(String str1, String str2) {
-        if (str1 == null) {
+        if (null == str1) {
             str1 = "";
         }
-        if (str2 == null) {
+        if (null == str2) {
             str2 = "";
         }
         return str1.equals(str2);
     }
+
     // 获取文件扩展名
     public static String extname(String path) {
         int index = path.lastIndexOf(".");
@@ -118,38 +149,125 @@ public class m {
         }
         return path.substring(index);
     }
+
     // 判断url是否是media
     public static Boolean isMedia(String url) {
+        return test(url, "^/[a-z0-9]{24}(\\.[^/]+)?");
+    }
+
+    // 内部函数 判断url是否是完整的media
+    public static Boolean _isMediaItem(String url) {
         return test(url, "^/[a-z0-9]{24}(\\.[^/]+)?$");
     }
+
     //从url获取media的Id
-    public static String getMediaId (String url) {
+    public static String getMediaId(String url) {
         return url.replaceAll("^/([a-z0-9]{24})(\\.[^/]+)?$", "$1");
     }
-    // 从url获取meida的名称
-    public static String getMediaName (String url) {
+
+    // 内部函数 获取media的Id数组
+    public static List<String> _getMediaIdList (List<String> list, String line) {
+        if (null == line) {
+            return list;
+        }
+        String[] urls = line.split(",");
+        for (String url : urls) {
+            if (_isMediaItem(url)) {
+                list.add(getMediaId(url));
+            }
+        }
+        return list;
+    }
+
+    // 内部函数
+    public static String _getMediaName(String url) {
         if (null == url) {
             return null;
         }
-        String extname = m.extname(url);
+        String extname = extname(url);
         String id = url.replaceAll("http://localhost:3000", "").replaceAll(extname, "");
-        if (m.test(id, "^/[a-z0-9]{24}$")) {
+        if (test(id, "^/[a-z0-9]{24}$")) {
             return id + extname;
         }
         return url;
     }
-    // 通过meida的名称获取url
-    public static String getMediaUrl(String name) {
-        if (name == null) {
+
+    // 从url获取meida的名称
+    // getMediaName("url", "url, url")
+    public static String getMediaName(String url) {
+        if (null == url) {
             return null;
         }
-        return isMedia(name) ? "http://localhost:3000" + name : name;
+        if (test(url, ",")) {
+            String[] urls = url.split(",");
+            for (int i = 0; i < urls.length; i++) {
+                urls[i] = getMediaName(urls[i]);
+            }
+            return StringUtils.join(urls, ",");
+        }
+        return _getMediaName(url);
     }
+
+    // 通过meida的名称获取url
+    public static String getMediaUrl(String name) {
+        if (null == name) {
+            return null;
+        }
+        String urls[] = name.split(",");
+        for (int i = 0; i < urls.length; i++) {
+            urls[i] = isMedia(urls[i]) ? Settings.apiServer + urls[i] : urls[i];
+        }
+        return StringUtils.join(urls, ",");
+    }
+
     // 获取默认密码
-    public static String getDefaultPassWord(String ...args) {
+    public static String getDefaultPassWord(String... args) {
         if (null == args[1]) {
             return args[0].substring(5);
         }
         return args[1];
+    }
+
+    // media 增加引用
+    // incRef("url", "url,url")
+    public static void incRef(Object... args) {
+       List<String> list = new ArrayList<>();
+        for (Object arg : args) {
+            _getMediaIdList(list, (String) arg);
+        }
+        System.out.println(list);
+    }
+
+    // media 更新引用
+    // replaceRef("incUrl", "decUrl", "incUrl,incUrl", "decUrl,decUrl")
+    public static void replaceRef(Object... args) {
+        List<String> incList = new ArrayList<>();
+        List<String> decList = new ArrayList<>();
+
+        int count = args.length - 2;
+        for (int i = 0; i <= count; i += 2) {
+            if (!eq((String)args[i], "")) {
+                _getMediaIdList(incList, (String) args[i]);
+                _getMediaIdList(decList, (String) args[i+i]);
+            }
+        }
+        System.out.println(incList);
+        System.out.println(decList);
+    }
+
+    // media 减少引用
+    // decRef("url", "url,url", ["url", "url,url"])
+    public static void decRef(Object... args) {
+        List<String> list = new ArrayList<>();
+        for (Object arg : args) {
+            if (arg instanceof List) {
+                for (String url : (List<String>) arg) {
+                    _getMediaIdList(list, url);
+                }
+            } else {
+                _getMediaIdList(list, (String) arg);
+            }
+        }
+        System.out.println(list);
     }
 }
